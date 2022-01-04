64a5ca0a5ddc6cb4ca2eac4dd7ae53276d7bbc0d
Hasan Turken <turkenh@gmail.com>
2022-01-04 18:18:20 +0300

    Add config and examples for redis enterprise
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- config/redis/config.go
+++ config/redis/config.go
@@ -17,6 +17,12 @@ limitations under the License.
 package redis
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+
 	"github.com/crossplane-contrib/terrajet/pkg/config"
 
 	"github.com/crossplane-contrib/provider-jet-azure/apis/rconfig"
@@ -77,4 +83,70 @@ func Configure(p *config.Provider) {
 		r.UseAsync = true
 		r.ExternalName = config.IdentifierFromProvider
 	})
+
+	p.AddResourceConfigurator("azurerm_redis_enterprise_cluster", func(r *config.Resource) {
+		r.References = config.References{
+			"resource_group_name": config.Reference{
+				Type: rconfig.APISPackagePath + "/azure/v1alpha1.ResourceGroup",
+			},
+		}
+		r.UseAsync = true
+		r.ExternalName = config.NameAsIdentifier
+		r.ExternalName.GetExternalNameFn = common.GetNameFromFullyQualifiedID
+		// /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/group1/providers/Microsoft.Cache/redisEnterprise/cluster1
+		r.ExternalName.GetIDFn = common.GetFullyQualifiedIDFn("Microsoft.Cache",
+			"redisEnterprise", "name",
+		)
+	})
+
+	p.AddResourceConfigurator("azurerm_redis_enterprise_database", func(r *config.Resource) {
+		r.References = config.References{
+			"resource_group_name": config.Reference{
+				Type: rconfig.APISPackagePath + "/azure/v1alpha1.ResourceGroup",
+			},
+			"cluster_id": config.Reference{
+				Type:      "EnterpriseCluster",
+				Extractor: rconfig.ExtractResourceIDFuncPath,
+			},
+		}
+		r.UseAsync = true
+		r.ExternalName = config.NameAsIdentifier
+		r.ExternalName.GetExternalNameFn = common.GetNameFromFullyQualifiedID
+		// /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/group1/providers/Microsoft.Cache/redisEnterprise/cluster1/databases/database1
+		r.ExternalName.GetIDFn = func(ctx context.Context, externalName string, parameters map[string]interface{}, providerConfig map[string]interface{}) (string, error) {
+			subIDStr, err := getStr(providerConfig, "subscription_id")
+			if err != nil {
+				return "", err
+			}
+			rgStr, err := getStr(parameters, "resource_group_name")
+			if err != nil {
+				return "", err
+			}
+			cidStr, err := getStr(parameters, "cluster_id")
+			if err != nil {
+				return "", err
+			}
+			cidParts := strings.Split(cidStr, "/")
+			cStr := cidParts[len(cidParts)-1]
+			nStr, err := getStr(parameters, "name")
+			if err != nil {
+				return "", err
+			}
+
+			return fmt.Sprintf("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Cache/redisEnterprise/%s/databases/%s",
+				subIDStr, rgStr, cStr, nStr), nil
+		}
+	})
+}
+
+func getStr(from map[string]interface{}, key string) (string, error) {
+	out, ok := from[key]
+	if !ok {
+		return "", errors.Errorf(common.ErrFmtNoAttribute, key)
+	}
+	outStr, ok := out.(string)
+	if !ok {
+		return "", errors.Errorf(common.ErrFmtUnexpectedType, key)
+	}
+	return outStr, nil
 }
