6a4cce53a8565e726a1c145c13ce579ad3312fcb
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2022-01-08 01:04:58 +0300

    Generate all resources for the v0.6.0-preview release
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- apis/network/v1alpha2/zz_generated.managedlist.go
+++ apis/network/v1alpha2/zz_generated.managedlist.go
@@ -19,8 +19,26 @@ package v1alpha2
 
 import resource "github.com/crossplane/crossplane-runtime/pkg/resource"
 
+// GetItems of this IPGroupList.
+func (l *IPGroupList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
 // GetItems of this LoadBalancerList.
 func (l *LoadBalancerList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
+// GetItems of this NetworkInterfaceList.
+func (l *NetworkInterfaceList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
 	for i := range l.Items {
 		items[i] = &l.Items[i]
