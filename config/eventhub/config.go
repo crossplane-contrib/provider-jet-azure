10f3b7507d87e409de22133fc791052b44303749
Yury Tsarev <yury@upbound.io>
2022-02-28 16:25:33 +0100

    Address PR review
    Signed-off-by: Yury Tsarev <yury@upbound.io>

--- config/eventhub/config.go
+++ config/eventhub/config.go
@@ -41,7 +41,6 @@ func Configure(p *config.Provider) {
 
 	p.AddResourceConfigurator("azurerm_eventhub", func(r *config.Resource) {
 		r.Version = common.VersionV1Alpha2
-		r.Kind = "EventHub"
 		r.References = config.References{
 			"namespace_name": config.Reference{
 				Type: "EventHubNamespace",
@@ -52,14 +51,13 @@ func Configure(p *config.Provider) {
 		r.ExternalName.GetExternalNameFn = common.GetNameFromFullyQualifiedID
 		// /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/group1/providers/Microsoft.EventHub/namespaces/namespace1/eventhubs/eventhub1
 		r.ExternalName.GetIDFn = common.GetFullyQualifiedIDFn("Microsoft.Eventhub",
-			"namespaces", "name",
+			"namespaces", "namespace_name",
 			"eventhubs", "name",
 		)
 	})
 
 	p.AddResourceConfigurator("azurerm_eventhub_consumer_group", func(r *config.Resource) {
 		r.Version = common.VersionV1Alpha2
-		r.Kind = "ConsumerGroup"
 		r.References = config.References{
 			"namespace_name": config.Reference{
 				Type: "EventHubNamespace",
@@ -73,15 +71,14 @@ func Configure(p *config.Provider) {
 		r.ExternalName.GetExternalNameFn = common.GetNameFromFullyQualifiedID
 		// /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/group1/providers/Microsoft.EventHub/namespaces/namespace1/eventhubs/eventhub1/consumerGroups/consumerGroup1
 		r.ExternalName.GetIDFn = common.GetFullyQualifiedIDFn("Microsoft.Eventhub",
-			"namespaces", "name",
-			"eventhubs", "name",
+			"namespaces", "namespace_name",
+			"eventhubs", "eventhub_name",
 			"consumergroups", "name",
 		)
 	})
 
 	p.AddResourceConfigurator("azurerm_eventhub_authorization_rule", func(r *config.Resource) {
 		r.Version = common.VersionV1Alpha2
-		r.Kind = "AuthorizationRule"
 		r.References = config.References{
 			"namespace_name": config.Reference{
 				Type: "EventHubNamespace",
@@ -95,8 +92,8 @@ func Configure(p *config.Provider) {
 		r.ExternalName.GetExternalNameFn = common.GetNameFromFullyQualifiedID
 		// /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/group1/providers/Microsoft.EventHub/namespaces/namespace1/eventhubs/eventhub1/authorizationRules/rule1
 		r.ExternalName.GetIDFn = common.GetFullyQualifiedIDFn("Microsoft.Eventhub",
-			"namespaces", "name",
-			"eventhubs", "name",
+			"namespaces", "namespace_name",
+			"eventhubs", "eventhub_name",
 			"authorizationRules", "name",
 		)
 	})
