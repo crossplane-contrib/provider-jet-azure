255ac38b49674e258f8c1516ccc89b3869b295e7
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2022-05-17 14:24:55 +0200

    Generate all resources for the v0.10.0-preview release
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- config/provider.go
+++ config/provider.go
@@ -59,7 +59,7 @@ const (
 //go:embed schema.json
 var providerSchema string
 
-var includedResources = []string{
+var _ = []string{
 	// "azurerm_.+",
 	"azurerm_virtual_.+",
 	"azurerm_kubernetes_.+",
@@ -113,6 +113,7 @@ var skipList = []string{
 	"azurerm_virtual_machine_extension",
 	"azurerm_virtual_machine_data_disk_attachment",
 	"azurerm_virtual_machine_scale_set_extension",
+	"azurerm_devspace_controller",
 	// irrelevant
 	"azurerm_virtual_desktop_application",
 	"azurerm_virtual_desktop_host_pool",
@@ -144,7 +145,7 @@ func GetProvider() *tjconfig.Provider {
 	pc := tjconfig.NewProviderWithSchema([]byte(providerSchema), resourcePrefix, modulePath,
 		tjconfig.WithShortName("azurejet"),
 		tjconfig.WithRootGroup("azure.jet.crossplane.io"),
-		tjconfig.WithIncludeList(includedResources),
+		// tjconfig.WithIncludeList(includedResources),
 		tjconfig.WithSkipList(skipList),
 		tjconfig.WithDefaultResourceFn(defaultResource(externalNameConfig(), groupOverrides())),
 	)
